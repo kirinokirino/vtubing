617cf7677143e986e6456ab5c83045ad30570d16
kirinokirino <helloworldagain2@gmail.com>
2024-11-11 13:33:22 +0100

    ImagePreprocessor

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ pub fn main() {
     let path = "/home/k/Documents/Rust/k/vtubing/onnx/lib/libonnxruntime.so.1.20.0";
     ort::init_from(path).commit().unwrap();
 
-    let model = Session::builder()
+    let detection = Session::builder()
         .unwrap()
         .with_optimization_level(GraphOptimizationLevel::Level3)
         .unwrap()
@@ -37,64 +37,23 @@ pub fn main() {
 
     let std_224 = [0.01712475, 0.017507, 0.01742919];
     let mean_224 = [-2.117904, -2.0357141, -1.8044444];
-    let mut frame = Mat::default();
-    let mut resized = Mat::default();
-    let mut rgb = Mat::default();
-    let mut normalized = Mat::default();
+    let mut preprocessor = ImagePreprocessor::new(std_224, mean_224);
     loop {
         // Capture a frame from the video capture
-        capture.read(&mut frame).unwrap();
-        if frame.empty() {
+        capture.read(&mut preprocessor.frame).unwrap();
+        if preprocessor.frame.empty() {
             eprintln!("Error: No frame captured.");
             continue;
         }
 
-        imgproc::resize(
-            &frame,
-            &mut resized,
-            Size2i::new(224, 224),
-            0.0,
-            0.0,
-            imgproc::INTER_LINEAR,
-        )
-        .unwrap();
-
-        // Step 2: Convert from BGR to RGB by reversing channels
-        imgproc::cvt_color(&resized, &mut rgb, imgproc::COLOR_BGR2RGB, 0).unwrap();
-
-        // Step 3: Normalize the image by applying the mean and std deviation
-        // We need to cast the Mat to a type that can hold the resulting floating point values (CV_32F)
-        rgb.convert_to(&mut normalized, CV_32FC1, 1.0, 0.0).unwrap();
-
-        // Normalize using (pixel - mean) / std
-        for row in 0..normalized.rows() {
-            for col in 0..normalized.cols() {
-                let pixel: &mut VecN<f32, 3> = normalized.at_2d_mut(row, col).unwrap();
-                for channel in 0..3usize {
-                    pixel[channel] = (pixel[channel] - mean_224[channel]) / std_224[channel];
-                }
-            }
-        }
-
-        // Convert `Mat` to `ndarray::Array4`
-        let array = mat_to_ndarray(&normalized);
-        let array = array.unwrap();
-
-        // Step 1: Expand dimensions (add a batch dimension)
-        let expanded = array.insert_axis(ndarray::Axis(0)); // Adds a batch dimension at axis 0
-
-        // Step 2: Transpose to change shape from (batch_size, height, width, channels)
-        // to (batch_size, channels, height, width)
-        let transposed = expanded.permuted_axes([0, 1, 2, 3]);
-
-        let inputs = transposed;
-        let outputs = model.run(ort::inputs!("input" => inputs).unwrap()).unwrap();
-        let predictions = outputs["output0"].try_extract_tensor::<f32>().unwrap();
-        //ort::inputs![]
-        // let outputs = model.run(ort::inputs!["image" => image]?)?;
-        // let predictions = outputs["output0"].try_extract_tensor::<f32>()?;
-
-        println!("Frame captured: {:?}", frame);
+        let input = preprocessor.preprocess();
+        
+        let output = detection
+            .run(ort::inputs!("input" => input).unwrap())
+            .unwrap();
+        let predictions = output["output"].try_extract_tensor::<f32>().unwrap();
+        println!("{}", predictions.len());
+        //println!("Frame captured: {:?}", frame);
     }
 
     // Close the video capture and window
@@ -132,34 +91,71 @@ fn mat_to_ndarray(mat: &Mat) -> Result<ndarray::Array3<f32>> {
     }
 }
 
-/// Function to convert an OpenCV `Mat` to an `ndarray::Array2`
-fn mat_to_array2(mat: &Mat) -> Result<Array2<f32>> {
-    // Ensure the Mat is of the expected type (CV_32F)
-    if mat.typ() != CV_32F {
-        return Err(opencv::Error::new(
-            opencv::core::StsBadArg,
-            format!(
-                "Mat type must be CV_32F for conversion to Array2, found: {}",
-                mat.typ()
-            ),
-        ));
+struct ImagePreprocessor {
+    std_224: [f32; 3],
+    mean_224: [f32; 3],
+    pub frame: Mat,
+    resized: Mat,
+    rgb: Mat,
+    normalized: Mat,
+}
+
+impl ImagePreprocessor {
+    fn new(std_224: [f32; 3], mean_224: [f32; 3]) -> Self {
+        let frame = Mat::default();
+        let resized = Mat::default();
+        let rgb = Mat::default();
+        let normalized = Mat::default();
+        Self {
+            std_224,
+            mean_224,
+            frame,
+            resized,
+            rgb,
+            normalized,
+        }
     }
 
-    // Get the number of rows and columns from the Mat
-    let rows = mat.rows();
-    let cols = mat.cols();
+    fn preprocess(&mut self) -> ndarray::Array4<f32> {
+        imgproc::resize(
+            &self.frame,
+            &mut self.resized,
+            Size2i::new(224, 224),
+            0.0,
+            0.0,
+            imgproc::INTER_LINEAR,
+        )
+        .unwrap();
 
-    // Get a flat slice of the Mat's data
-    let mat_data = mat.data_typed::<f32>().unwrap();
+        // Step 2: Convert from BGR to RGB by reversing channels
+        imgproc::cvt_color(&self.resized, &mut self.rgb, imgproc::COLOR_BGR2RGB, 0).unwrap();
 
-    // Convert the flat data into an ndarray::Array2
-    let array = Array2::<f32>::from_shape_vec((rows as usize, cols as usize), mat_data.to_vec());
+        // Step 3: Normalize the image by applying the mean and std deviation
+        // We need to cast the Mat to a type that can hold the resulting floating point values (CV_32F)
+        self.rgb
+            .convert_to(&mut self.normalized, CV_32FC1, 1.0, 0.0)
+            .unwrap();
 
-    match array {
-        Ok(array) => Ok(array),
-        Err(err) => Err(opencv::Error {
-            code: opencv::core::StsError,
-            message: format!("ShapeError: {err}"),
-        }),
+        // Normalize using (pixel - mean) / std
+        for row in 0..self.normalized.rows() {
+            for col in 0..self.normalized.cols() {
+                let pixel: &mut VecN<f32, 3> = self.normalized.at_2d_mut(row, col).unwrap();
+                for channel in 0..3usize {
+                    pixel[channel] =
+                        (pixel[channel] - self.mean_224[channel]) / self.std_224[channel];
+                }
+            }
+        }
+
+        // Convert `Mat` to `ndarray::Array4`
+        let array = mat_to_ndarray(&self.normalized).unwrap();
+
+        // Step 1: Expand dimensions (add a batch dimension)
+        let expanded = array.insert_axis(ndarray::Axis(0)); // Adds a batch dimension at axis 0
+
+        // Step 2: Transpose to change shape from (batch_size, height, width, channels)
+        // to (batch_size, channels, height, width)
+        let transposed = expanded.permuted_axes([0, 1, 2, 3]);
+        transposed
     }
 }
